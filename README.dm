51a69258daef2462112929bf032794d0b118a299
Magnus <magn902m@stud.kea.dk>
2022-02-10 14:14:28 +0100

    bar chart is working but not moving

---
+++ README.dm
@@ -0,0 +1,1 @@
+Link to bar chart: https://magn902m.github.io/bar_chart/
--- script.js
+++ script.js
@@ -1,7 +1,54 @@
 `use strict`;
 
-window.addEventListener("DOMContentLoaded", start);
+window.addEventListener("DOMContentLoaded", init);
 
-function start() {
-  console.log("start");
+// let arrayCounter;
+// let array = 1;
+const model = [
+  8, 12, 32, 4, 13, 24, 23, 22, 1, 0, 30, 7, 3, 6, 3, 19, 24, 29, 14, 16, 3, 1, 9, 8, 30, 12, 0, 30,
+  7, 3, 6, 3, 6, 3, 19, 24, 29, 14, 4, 1,
+];
+const queueSize = getNumberOfCustomers();
+const barArray = document.querySelectorAll(".bar");
+
+displayData();
+
+function init() {
+  console.log("init");
+  //   console.log(barArray);
+  //   for (let arrayCounter; arrayCounter <= 40; arrayCounter++) {
+  //     array = Math.floor(Math.random() * 1);
+  //     console.log(array);
+  //   https://www.w3resource.com/javascript-exercises/javascript-array-exercise-35.php
+  //   }
+}
+
+function loop() {
+  console.log("loop");
+}
+
+function getNumberOfCustomers() {
+  console.log("getNumberOfCustomers");
+  return Math.floor(Math.random() * 23);
+}
+
+function displayData() {
+  console.log("displayData");
+  barArray.forEach((elm, i) => {
+    elm.textContent = model[i];
+    // elm = model[i];
+
+    console.log(elm);
+    // barArray[11].style.height = "50%";
+    barArray[i].style.height = `${model[i]}px`;
+  });
+}
+
+function modifyModel() {
+  console.log("ModifyModel");
+  //   for (let counter = 0; counter <= 40; counter++) {
+  //     console.log(counter);
+  //   }
+  //   div.style.height = "50%";
+  //   model.style.height = ();
 }

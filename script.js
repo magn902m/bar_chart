e4b1ad97e2f14044e535d1089f068cc00833e5fa
Magnus <magn902m@stud.kea.dk>
2022-02-10 21:29:10 +0100

    got the bar chart to move and remove element, but not adding new

--- script.js
+++ script.js
@@ -2,8 +2,6 @@
 
 window.addEventListener("DOMContentLoaded", init);
 
-// let arrayCounter;
-// let array = 1;
 const model = [
   8, 12, 32, 4, 13, 24, 23, 22, 1, 0, 30, 7, 3, 6, 3, 19, 24, 29, 14, 16, 3, 1, 9, 8, 30, 12, 0, 30,
   7, 3, 6, 3, 6, 3, 19, 24, 29, 14, 4, 1,
@@ -11,7 +9,10 @@ const model = [
 const queueSize = getNumberOfCustomers();
 const barArray = document.querySelectorAll(".bar");
 
-displayData();
+let timeoutSpeed = 500;
+let counter = 0;
+
+// displayData();
 
 function init() {
   console.log("init");
@@ -21,34 +22,83 @@ function init() {
   //     console.log(array);
   //   https://www.w3resource.com/javascript-exercises/javascript-array-exercise-35.php
   //   }
+  //   loop();
 }
 
 function loop() {
-  console.log("loop");
+  //   console.log("loop");
+  console.log(counter);
+  if (counter < 40) {
+    // console.log(counter < 39);
+    model.unshift(queueSize);
+    counter++;
+    // displayData();
+  } else {
+    model.length = 39;
+    model.unshift(queueSize);
+  }
+  console.log(model);
+  setTimeout(displayData, timeoutSpeed);
 }
+
+function displayData() {
+  console.log("displayData");
+  for (let i = 0; i < 40; i++) {
+    // console.log(i);
+    barArray.textContent = model[i];
+    // console.log(barArray[i]);
+    barArray[i].style.height = `${model[i]}px`;
+  }
+
+  setTimeout(loop, timeoutSpeed);
+  //   loop();
+
+  //   barArray.forEach((elm, i) => {
+  //     elm.textContent = model[i];
+  //     // elm = model[i];
+  //     console.log(elm);
+  //     barArray[i].style.height = `${model[i]}px`;
+  //     // loop();
+  //   });
+}
+
+//   console.log("tallet til 40");
+//   displayData();
+//   setTimeout(loop, 3000);
+
+//   model.unshift(counter);
+//   setTimeout(loop, 3000);
+//   console.log("Tal: ", counter);
+
+// if (i < 39) {
+// model.unshift(queueSize);
+// i++;
+// } else {
+// model.length = 39;
+// model.unshift(queueSize);
+// }
+
+// function loop() {
+//     //   console.log("loop");
+//     let counter = 0;
+//     counter++;
+//     if (counter < 40) {
+//       // counter - 2;
+//       model.pop(counter);
+//       console.log("tallet til 40");
+//       displayData();
+//       setTimeout(loop, 3000);
+//     }
+//     model.unshift(counter);
+//     setTimeout(loop, 3000);
+//     console.log("Tal: ", counter);
+//   }
 
 function getNumberOfCustomers() {
   console.log("getNumberOfCustomers");
   return Math.floor(Math.random() * 23);
 }
 
-function displayData() {
-  console.log("displayData");
-  barArray.forEach((elm, i) => {
-    elm.textContent = model[i];
-    // elm = model[i];
-
-    console.log(elm);
-    // barArray[11].style.height = "50%";
-    barArray[i].style.height = `${model[i]}px`;
-  });
-}
-
 function modifyModel() {
   console.log("ModifyModel");
-  //   for (let counter = 0; counter <= 40; counter++) {
-  //     console.log(counter);
-  //   }
-  //   div.style.height = "50%";
-  //   model.style.height = ();
 }

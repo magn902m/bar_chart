d586c47da6691ba288a3025c13e94f58a1b9cbac
Magnus <magn902m@stud.kea.dk>
2022-02-11 09:19:11 +0100

    script working with comments

--- script.js
+++ script.js
@@ -2,64 +2,55 @@
 
 window.addEventListener("DOMContentLoaded", init);
 
-const model = [
-  8, 12, 32, 4, 13, 24, 23, 22, 1, 0, 30, 7, 3, 6, 3, 19, 24, 29, 14, 16, 3, 1, 9, 8, 30, 12, 0, 30,
-  7, 3, 6, 3, 6, 3, 19, 24, 29, 14, 4, 1,
-];
-const queueSize = getNumberOfCustomers();
+// const model = [
+//   8, 12, 32, 4, 13, 24, 23, 22, 1, 0, 30, 7, 3, 6, 3, 19, 24, 29, 14, 16, 3, 1, 9, 8, 30, 12, 0, 30,
+//   7, 3, 6, 3, 6, 3, 19, 24, 29, 14, 4, 1,
+// ];
+
+// const queueSize = getNumberOfCustomers();
+const model = [];
 const barArray = document.querySelectorAll(".bar");
 
 let timeoutSpeed = 500;
-let counter = 0;
-
-// displayData();
 
 function init() {
   console.log("init");
-  //   console.log(barArray);
-  //   for (let arrayCounter; arrayCounter <= 40; arrayCounter++) {
-  //     array = Math.floor(Math.random() * 1);
-  //     console.log(array);
-  //   https://www.w3resource.com/javascript-exercises/javascript-array-exercise-35.php
-  //   }
-  //   loop();
+  loop();
 }
 
+// Controller
 function loop() {
-  //   console.log("loop");
-  console.log(counter);
-  if (counter < 40) {
-    // console.log(counter < 39);
-    model.unshift(queueSize);
-    counter++;
-    // displayData();
+  //   console.log("loop")
+  arrUpdate();
+  displayData();
+  setTimeout(loop, timeoutSpeed);
+}
+
+// Model
+function arrUpdate() {
+  if (model.length < 40) {
+    model.unshift(getNumberOfCustomers());
   } else {
     model.length = 39;
-    model.unshift(queueSize);
+    model.unshift(getNumberOfCustomers());
   }
   console.log(model);
-  setTimeout(displayData, timeoutSpeed);
 }
 
+// View
 function displayData() {
-  console.log("displayData");
+  // console.log("displayData");
   for (let i = 0; i < 40; i++) {
     // console.log(i);
     barArray.textContent = model[i];
     // console.log(barArray[i]);
     barArray[i].style.height = `${model[i]}px`;
   }
+}
 
-  setTimeout(loop, timeoutSpeed);
-  //   loop();
-
-  //   barArray.forEach((elm, i) => {
-  //     elm.textContent = model[i];
-  //     // elm = model[i];
-  //     console.log(elm);
-  //     barArray[i].style.height = `${model[i]}px`;
-  //     // loop();
-  //   });
+function getNumberOfCustomers() {
+  // console.log("getNumberOfCustomers");
+  return Math.floor(Math.random() * 32);
 }
 
 //   console.log("tallet til 40");
@@ -93,12 +84,3 @@ function displayData() {
 //     setTimeout(loop, 3000);
 //     console.log("Tal: ", counter);
 //   }
-
-function getNumberOfCustomers() {
-  console.log("getNumberOfCustomers");
-  return Math.floor(Math.random() * 23);
-}
-
-function modifyModel() {
-  console.log("ModifyModel");
-}
